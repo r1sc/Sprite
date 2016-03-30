43c3dce2937248e2b08f8545b43e0be93f809b6b
Erik Sandberg <sandberg.erik@gmail.com>
2016-03-30 13:58:11 +0200

    Updated old variable names in the sample

--- Sprite.Sample/Program.cs
+++ Sprite.Sample/Program.cs
@@ -6,30 +6,30 @@ namespace Sprite.Sample
     {
         static void Main(string[] args)
         {
-            var gfx = new SpriteWindow("Test");
+            var window = new SpriteWindow("Test");
             var glBitmap = SpriteBitmap.FromImage(Properties.Resources.test, 4, 4);
             var i = 0;
-            while (gfx.Running)
+            while (window.Running)
             {
                 for (int y = 0; y < 25; y++)
                 {
                     for (int x = 0; x < 40; x++)
                     {
-                        gfx.DrawGlBitmap(glBitmap, x * 8, y * 8, 5);
+                        window.DrawGlBitmap(glBitmap, x * 8, y * 8, 5);
                     }
                 }
 
-                if (gfx.IsKeyDown(77))
+                if (window.IsKeyDown(77))
                 {
                     i++;
                 }
-                if (gfx.IsKeyDown(72))
+                if (window.IsKeyDown(72))
                 {
                     i--;
                 }
 
-                gfx.DrawGlBitmap(glBitmap, 12+i, 15, 0);
-                gfx.Swap();
+                window.DrawGlBitmap(glBitmap, 12+i, 15, 0);
+                window.Swap();
             }
         }
     }
